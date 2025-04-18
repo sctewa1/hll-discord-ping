2db9a4fe7a5c5ea607885ec303589f2ce859b579
sctewa1 <gregwood@gmail.com>
2025-04-18 13:47:29 +1000

    Update logging_config.py

--- ping_setter/management/commands/logging_config.py
+++ ping_setter/management/commands/logging_config.py
@@ -21,29 +21,33 @@ def setup_logging():
     # Get the timezone object
     local_tz = timezone(TIMEZONE)
     
-    # Calculate 3 AM in the local timezone
+    # Calculate next 3 AM in local timezone
     now = datetime.now(local_tz)
     next_run = now.replace(hour=3, minute=0, second=0, microsecond=0)
-
     if now > next_run:
-        # If it's already past 3 AM, schedule the log rotation for the next day
         next_run += timedelta(days=1)
 
-    # Create a TimedRotatingFileHandler to rotate logs at 3 AM
+    # Create a TimedRotatingFileHandler to rotate logs daily at 3 AM
     handler = TimedRotatingFileHandler(
-        log_filename, when="midnight", interval=1, backupCount=50, atTime=next_run.time()
+        log_filename,
+        when="midnight",
+        interval=1,
+        backupCount=50,
+        atTime=next_run.time()
     )
 
-    # Set the suffix for archived log files
-    handler.suffix = "%Y-%m-%d"  # Format the archived log file names with the date (e.g., discord_bot-2025-04-17.log)
+    # Format log lines with timestamp and level
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M.%f'
+    )
+    formatter.default_msec_format = '%s.%03d'
 
-    # Create a formatter and set it for the handler
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
 
     # Set up the logger
     logger = logging.getLogger('discord_bot')
-    logger.setLevel(logging.INFO)  # Set the desired log level
+    logger.setLevel(logging.INFO)
     logger.addHandler(handler)
 
     return logger
