541545c35c52082879c18208687ee090e4abb1ee
sctewa1 <gregwood@gmail.com>
2025-04-19 22:19:25 +1000

    Update send_ping.py

--- ping_setter/management/commands/send_ping.py
+++ ping_setter/management/commands/send_ping.py
@@ -3,6 +3,7 @@ import os
 import logging
 import requests
 import discord
+import asyncio
 from discord.ext import commands
 from discord import app_commands
 from django.core.management.base import BaseCommand
